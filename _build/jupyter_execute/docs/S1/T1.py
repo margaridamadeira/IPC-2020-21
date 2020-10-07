3c34ba14353361246d61576d523e433d2bd0a7a6
MMMM <32166069+mmcsamcm@users.noreply.github.com>
2020-10-07 19:27:19 +0100

    S1 - usando notebooks

--- _build/jupyter_execute/docs/S1/T1.py
+++ _build/jupyter_execute/docs/S1/T1.py
@@ -1,102 +1,125 @@
 # Breve apresentação de Python
 
-## O IPyhton como interpretador de comandos
+## O IPython como interpretador de comandos
 
 O ambiente IPython, simples e interactivo, é extremamente conveniente para a aprendizagem da programação em Python, pois permite a elaboração de pequenos programas e sua experimentação. 
 
 Na maioria dos tutoriais, a primeira proposta de utilização do Python é como [calculadora](https://docs.python.org/3/tutorial/introduction.html) simples. 
 
 Isto porque o Python disponibiliza à partida, para além de algumas funcionalidades básicas, várias operações numéricas que podemos utilizar:
-- `+` (adição),
-- `-` (subtracção),
-- `*` (multiplicação), 
-- `/` (divisão),
-- `**` (exponenciação),
-- `//` (divisão inteira), 
-- `%` (resto da divisão inteira). 
 
+> `+` (adição)
+
+> `-` (subtração)
+
+> `*` (multiplicação)
+
+> `/` (divisão)
+
+> `**` (exponenciação)
+
+> `//` (divisão inteira)
+
+> `%` (resto da divisão inteira)
+
+A utilização das operações de adição, subtração, multiplicação e divisão é muito simples.
+O único cuidado que temos que ter é lembrarmo-nos da prioridade das operações e usar parenteses se for caso disso.
 Vejamos alguns exemplos.
 
-1+1-2
+5+4*2
 
-(2*3)%5
+(5+4)*2
 
-7/3
+A exponenciação é usada para calcular potências.
 
-7//3
+2**3 
 
-10**3
+5**2
 
-1/3+1/2
+A divisão inteira e resto da divisão inteira merece alguma atenção. 
+Na divisão *normal*, teriamos
 
-Existem funções, disponíveis em Python, que iremos conhecendo aos poucos.
+7/2
 
-abs(-1)*max(1,2,3)+min(1,2,3)+round(2.6)
+Na divisão inteira, o resultado é um número inteiro e poderá haver resto
 
-É de notar que existem numerosas e diversas bibliotecas com extensões à linguagem básica, que servem os mais diversos fins. Há bibliotecas que dão suporte a mecanismos de cálculo e visualização gráfica que poderão ser bastante úteis, nomeadamente: Scipy e Matplotlib, ambas incluindo Numpy, que por sua vez inclui Math, e ainda Sympy, cuja utilização ilustramos de seguida.
+7//2
+
+7%2
+
+Existem funções, disponíveis em Python, que iremos conhecendo aos poucos. 
+Tente determinar o resultado antes de executar a célula.
+
+> abs(-1)*max(1, 2, 3) + min(4, 5, 6)
+
+Para executar a célUla abaixo, remova o carater '#' que indica um comentário.
+
+# abs(-1)*max(1, 2, 3) + min(4, 5, 6)
+
+Pode consultar a documentação, embora a forma exata possa variar consoante a ferramenta que está a usar (Spyder ou Jupyter). E frequentemente, há mais de uma maneira de obter essa documentação. Por exemplo,
+
+?abs
+
+abs?
+
+help(abs)
+
+## Uso de bibliotecas
+
+É de notar que existem numerosas e diversas bibliotecas com extensões à linguagem básica, que servem os mais diversos fins. Há bibliotecas que dão suporte a mecanismos de cálculo e visualização gráfica que poderão ser bastante úteis. 
 
 Nesta unidade curricular iremos por vezes usar alguns desses mecanismos. Iniciamos com a programação em Python e avançamos depois para a utilização das bibliotecas. 
 
-As extensões Numpy e Matplotlib estão incluídas na extensão Pylab, que pode ser carregada como se mostra abaixo. A instrução precedida de % é aquilo a que na terminologia do IPython se denomina $\textrm{magic}$, uma extensão à linguagem Python que facilita a interacção com o sistema. Introduziremos, quando necessário, outras magias do IPython.
+Um exemplo é *Math*.
 
-
-%pylab inline
-
-?pylab
-
-Avaliando a expressão acima e percorrendo a informação na janela que se abre podemos identificar as funcionalidades que passamos a ter disponíveis, algumas das quais ilustraremos de seguida. Poderá ser útil averiguar também os conteúdos de Numpy e Matplotlib, bem como da extensão Math que é incluída conjuntamente.
-
-?pi
+from math import *
 
 pi
 
-cos(2*pi)
+cos(pi)
 
-sin(pi/6)
-
-sin(2*pi/3)-sqrt(3)/2
+> *Sugestão*: Crie uma nova célula antes de importar *math* e tente obter o valor de $ \pi $, ou o $ cos(2\pi) $. A execução da célula vai falhar. Explique porquê.
 
 log(e**5)
 
 log2(1024)-log10(1000)
 
-Veremos mais tarde como fazer para trabalhar com precisão arbitrária, ou mesmo com representações simbólicas, exactas mas menos eficientes.
 
-### Vetores e matrizes
 
-Vetores e matrizes são designações usadas em matemática e, de forma simplificada, correspondem a uma forma de organizar coleções de números.
+Outro exemplo do que usaremos é *Matplotlib*.
 
-A manipulação numérica de vectores e matrizes é disponibilizada directamente pela extensão Pylab. Veremos isto mais tarde do ponto de vista da utilização na programação. O significado e importância de operações matriciais como produtos, inversas, determinantes ou valores próprios é objecto de estudo da disciplina de Álgebra Linear.
+?matplotlib
 
-### Números complexos
+No futuro, para se trabalhar com vetores e matrizes, poderíamos usar *NumPy* ou outras alternativas.
 
-complex(1,1)
+?numpy
 
-conjugate(1+1j)
+A instrução precedida de % é aquilo a que na terminologia do IPython se denomina *magic*, uma extensão à linguagem Python que facilita a interacção com o sistema. Introduziremos, quando necessário, outras magias do IPython.
 
-real(complex(1,-1))
+Neste caso e apenas para efeitos de demonstração, ativamos o Pylab. No futuro, em vez de trazermos a caixa de ferramentas toda, escolhemos a ferramenta que precisamos.
 
-imag(complex(1,-1))
+%pylab inline
 
-abs(1+1j)
+E já temos mais funcionalidades disponíveis, por exemplo, para gerar gráficos.
+
 
 ### Geração de gráficos
 A representação gráfica é uma parte importante e, felizmente, não é complicada.
-Vejamos como faríamos para representar a função $f(x) = x² + 1$ no intervalo $[-5,5]$.
+Vejamos como faríamos para representar a função $f(x) = x² $ quando o conjunto de partida é $ \{0, 1, 2, 3, 4, 5 \} $
 
-x=arange(-5,6,0.1)
+x = arange(6) # 
 
 
 # O que está afinal no eixo do x
 print (x)
 
-plot(x, x**2+1)
+plot(x, x**2)
 
 E como faríamos para representar as funções seno e coseno no mesmo gráfico?
 
 x=arange(0,10,0.1); 
 
-plot(x, sin(x), 'r', x, 1+cos(x),'k')
+plot(x, sin(x), 'r', x, 1+cos(x),'b')
 
 Há outros tipos de gráficos para além do *plot*, e quando chegarmos lá veremos melhor. Uma *espreitadela* rápida ao que seremos capazes de fazer.
 
@@ -124,4 +147,4 @@ legend()
 
 * O IPython é um ambiente de programação interactivo muito simples, apropriado à experimentação rápida de programas em Python, e portanto adequado à aprendizagem da programação
 * Munido das extensões adequadas o ambiente IPython pode ser utilizado como poderosa ferramenta de cálculo numérico e simbólico, e de visualização gráfica, extremamente útil em aplicações científicas, matemática e engenharia
-* A extensão Pylab fornece um ambiente de computação essencialmente equivalente ao popular sistema proprietário MATLAB, também muito usado em aplicações científicas. 
+* A biblioteca Matplotlib fornece um ambiente de computação essencialmente equivalente ao popular sistema proprietário MATLAB, também muito usado em aplicações científicas. 
