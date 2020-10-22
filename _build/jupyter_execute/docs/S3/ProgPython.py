60ad0670a3d123b572dadba9997dfe4eeb688d39
MMMM <32166069+mmcsamcm@users.noreply.github.com>
2020-10-22 09:07:13 +0100

    atualizar com S3 T

--- _build/jupyter_execute/docs/S3/ProgPython.py
+++ _build/jupyter_execute/docs/S3/ProgPython.py
@@ -8,18 +8,7 @@ Nesta lição, veremos de forma mais abrangente, a linguagem Python.
 
 1. Expressões com tipos de dados nativos simples, incluindo a indexação de strings
 1. Interrogações e ciclo *while*
-1. Funções nativas do python padrão
 
-
-print('x = {0:d}; y = {1:d}'.format(x, y))
-
-Mas o conteúdo não tem que ser sempre numérico. 
-
-nome = 'Maria'
-print(nome)
-
-apelido = 'Silva'
-apelido
 
 ### Expressões com tipos de dados nativos simples
 
@@ -39,18 +28,21 @@ As cadeias de carateres (vulgo, *strings*) armazenam sequências alfanuméricas.
 nome = 'Maria'
 apelido = 'Moura'
 nome + ' ' + apelido 
-print (nome, apelido)
+
+print (nome, apelido) 
 
 nome_completo = nome + ' ' + apelido 
 saudar = 'Bom dia,' + ' ' + nome_completo + '!'*3
 print(saudar)
 
-# Isto é muito importante, note que começa em 0
-nome[0]
-
 frase = 'Python é muito giro e interessante!'
 
-frase[0:7]
+# Isto é muito importante, note que começa em 0
+frase[0]
+
+frase[0:6]
+
+Os índices negativos podem ser entendidos como *a contar do fim*
 
 frase[-1]
 
@@ -58,28 +50,16 @@ frase[-13:-1]
 
 #### Operações com valores lógicos
 
+Os valores lógicos (de tipo *bool*) podem assumir os valores **False** e **True**. Usamos:
+
+    == para a igualdade. E não confunda com = da atribuição
+    != para a negação
+    not() também para a negação
+    or para conjunção
+    and para a disjunção
 
 
-True 
-
-False
-
-True == True
-
-True != True
-
-True != False
-
-True or False 
-
-True and False 
-
-not(True) 
-
-not(False) 
-
-
-##### Experimentemos
+##### Exemplos
 
 True 
 
@@ -120,49 +100,96 @@ True and False
 | True | False | True|
 | True | True | True |
 
-# Faça variar os valores de a e b e a operação lógica
+# Experimente: faça variar os valores de a e b e a operação lógica
 a = True; b = not a; a and b 
 
-#### Ainda expressões
-
-## Programação em Python 
-### I - Interrogações e ciclos
-
+### Interrogações e ciclo *while*
 
 É do conhecimento geral que os computadores fazem aquilo para que foram programados. Mas representar "coisas", dar-lhes nomes, e operar com/sobre elas, usando expressões, não é suficiente.
 
 As ações a desempenhar dependem das circunstâncias. Por isso, surgiram formas de definir o que vai ser, ou não, feito. Na base do controlo de execução estão a avaliação das circunstâncias pela interrogação de estados significativos e a decisão de prosseguir ou repetir.
+
+
 
 Vejamos então, duas formas de controlar o fluxo de execução.
 
     - if
         Se <algo se verifica> <faça-se>
         Se <acabou o filme> <sair da sala>
+        
     - while
         Enquanto <algo se verifica> <faça-se>
         Enquanto <o filme decorre> <silêncio>
 
-        
 #### Exemplos
 
-a = 6
-if (a%2)==0:
+Queremos apresentar uma mensagem a indicar se um número é maior, igual, ou menor que zero.
+
+Vamos esboçar a solução e melhorando progressivamente.
+
+
+A variável *num* representará o nosso número neste caso.
+
+num = 4
+
+Para começar, queremos a mensagem para indicar se o número é maior que zero 
+
+if (num > 0):
     # a identação é importante
-    print(a, 'é par')
+    print('{} é maior que zero'.format(num))
 
-a = 5
-if (a%2)==0:
-   print(a, 'é par')
-else:
-   print(a, 'não é par')
+Agora que isso está, acrescentamos outro caso. E para testar esse caso, mudamos o valor de *num*
 
-a = -4
-if (a == 0):
-    print('a é zero')
-elif (a > 0):
-    print ('a é positivo')
+num = -2
+
+if (num > 0):
+    print('{} é maior que zero'.format(num))
 else:
-    print('a é negativo')
+    print('{} não é maior que zero'.format(num))
+
+Mas não ser maior que zero, não é suficiente. 
+
+Agora precisávamos de outro teste para saber se é zero, ou menor que zero.
+
+num = 0 
+
+if (num > 0):
+    print('{} é maior que zero'.format(num))
+elif (num == 0):
+    print('{} é igual a zero'.format(num))
+else:
+    print('{} é menor que zero'.format(num))
+
+Vejamos outro exemplo: queremos saber se um número é par.
+
+Ora um número par é divisível por 2, ou seja, o resto da divisão por dois é zero.
+
+num = 6
+
+if (num%2) == 0:
+    # a identação é importante
+    print(num, 'é par')
+
+num = 5
+
+if (num%2) == 0:
+   print(num, 'é par')
+else:
+   print(num, 'não é par')
+
+Por vezes, queremos tratar toda uma gama de elementos, mas não sabemos quantos são.
+Sabemos, isso sim, em que condições queremos fazer esse "tratamento".
+
+Por exemplo, queremos apresentar os elementos $a$ do intervalo $ [0, 5[ $ sabendo que $a \in N$
+
+a = 0
+while (a < 5):
+    # a identação é importante
+    print (a)
+    a += 1 # O mesmo que a = a + 1
+
+
+Se quisessemos os valores de $[0, 3]$ por ordem decrescente, podíamos fazer
 
 a = 3
 while (a >= 0):
@@ -170,73 +197,39 @@ while (a >= 0):
     print (a)
     a -= 1 # O mesmo que a = a - 1
 
-## Programação em Python 
-### O - Organizar
+Criando expressões com estes operadores e conseguindo moldar o nosso programa às circunstâncias, já conseguimos fazer muitas coisas.
 
-Agora que já temos as ferramentas essenciais, precisamos de preparar a nossa solução computacional. Usamos as funções do Python e, quando não existirem, fazemos as funções necessárias. Várias funções podem ser reunidas para criar uma solução, o nosso programa.
+Vejamos agora o cálculo do fatorial de um número.
 
-Vamos começar com um programa _muito_ simples, tão simples que é desnecessário: quero somar dois números inteiros.
-Na resolução deste problema:
-+ precisamos saber quais são as parcelas (*input*) e
-+ precisamos de adicionar as parcelas para obtermos a soma (*processamento*)
-+ precisamos de apresentar uma mensagem com as parcelas e a soma resultante (*output*).
+Queremos calcular o fatorial de um número usando a expressão $n! = n * (n-1)!$, para  $n > 1$
 
-Temos pois que organizar a nossa solução na forma de um programa de forma que cumpra o seu objetivo mas com cada parte separada de forma a que possamos testar e, mais importante, possamos reutilizar noutros programas que venhamos a precisar.
-
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Authors: Margarida Madeira e Moura
-# Contacts: mmadeira@ualg.pt
-
-# Exemplo inicial de um programa
-def soma (x,y):
-	""" Função que devolve a soma de dois números.
-
-	Args:
-	    x (int): uma parcela
-	    y (int): outra parcela
-
-	Returns:
-	    int: a soma das duas parcelas
-
-	Examples:
-	    >>> soma (2, 3)
-	    5
-	    >>> soma (4, 5) 
-	    9
-	"""
-	return x + y
-
-def testar_soma():
-    """ O teste da função soma:
-    lê os dados, chama a função e escreve os resultados """
-    x = int(input())
-    y = int(input())
+def fatorial_r(n):
+    """Calculo do fatorial de um número.
+    n! = n * (n-1)! para n > 1
+    """
     
-    result = soma(x, y)
+    resultado = 1 # Elemento neutro da multiplicação 
+    if (n>1):
+        resultado = n * fatorial_r(n-1)
     
-    print ("A soma de",x,"com",y,"é",result)
+    return resultado
+    
 
-# Se o nosso programa for chamado diretamente a partir da consola 
-# é preciso indicar por onde se começa 
-if __name__ == "__main__":
-    testar_soma()
-    print("Feito!")
 
-## Programação em Python 
-### U - Usar
+print(fatorial_r(5))
 
-Porque o nosso programa está definido com várias funções podemos dar-lhe vários usos.
+Queremos agora calcular o fatorial de um número usando a expressão $n! = n * (n-1) * ... * 2$, para  $n > 0$
 
-Para além de o podermos ter no *notebook*, podemos guardá-lo num ficheiro de extensão .py e chamá-lo na *prompt*.
-    > python P1B.py
+def fatorial_i(n):
+    """Calculo do fatorial de um número.
+    n! = n * (n-1) * ... * 2
+    """
+    
+    resultado = 1 # Elemento neutro da multiplicação 
+    while (n>1):
+        resultado *= n 
+        n -= 1        
+    
+    return resultado
 
-Mas podemos também usar as funções que aqui definimos.
-
-testar_soma()
-# Compare com o resultado anterior. Porque é que é diferente?
-
-a, b = 4, 5 # Podemos fazer mais do que uma atribuição
-soma(a,b)   # Note que os argumentos a e b da função são diferentes 
-            # dos parâmetros x e y
+print(fatorial_i(5))
